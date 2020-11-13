8f98b4b290bb1c2c077b63374082d1bf0ac919e4
Kiwoong Andrew Kim <kw2446@gmail.com>
2020-11-12 22:19:32 -0500

    add gradle, utility, PreferenceManager

---
+++ app/src/main/java/mobilesdkdemo/rbbn/itswinter/utils/PreferenceManager.java
@@ -0,0 +1,130 @@
+package mobilesdkdemo.rbbn.itswinter.utils;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceManager {
+
+    public static final String PREFERENCES_NAME = "setting";
+
+    private static final String DEFAULT_VALUE_STRING = "";
+    private static final boolean DEFAULT_VALUE_BOOLEAN = false;
+    private static final int DEFAULT_VALUE_INT = -1;
+    private static final long DEFAULT_VALUE_LONG = -1L;
+    private static final float DEFAULT_VALUE_FLOAT = -1F;
+
+    private static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+
+    public static void setValue(Context context, String key, Object value) {
+        SharedPreferences prefs = getPreferences(context);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        if (value instanceof String) {
+            editor.putString(key, (String) value);
+        } else if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value);
+        }
+        editor.apply();
+
+    }
+
+
+    public static Object getValue(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getAll().get(key);
+    }
+
+    /**
+     * String value get
+     *
+     */
+
+    public static String getString(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getString(key, DEFAULT_VALUE_STRING);
+
+    }
+
+
+    /**
+     * boolean value get
+     *
+     */
+
+    public static boolean getBoolean(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getBoolean(key, DEFAULT_VALUE_BOOLEAN);
+
+    }
+
+
+    /**
+     * int value get
+     *
+     */
+
+    public static int getInt(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getInt(key, DEFAULT_VALUE_INT);
+
+    }
+
+
+    /**
+     * long value get
+     *
+     */
+
+    public static long getLong(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getLong(key, DEFAULT_VALUE_LONG);
+
+    }
+
+
+    /**
+     * float value get
+     *
+     */
+
+    public static float getFloat(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        return prefs.getFloat(key, DEFAULT_VALUE_FLOAT);
+    }
+
+
+    /**
+     *   remove the key
+     *
+     */
+
+    public static void removeKey(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.remove(key);
+        edit.apply();
+
+    }
+
+
+    /**
+     * remove all keys
+     *
+     */
+
+    public static void clear(Context context) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.clear();
+        edit.apply();
+    }
+
+}
---
+++ app/src/main/java/mobilesdkdemo/rbbn/itswinter/utils/Utility.java
@@ -0,0 +1,27 @@
+package mobilesdkdemo.rbbn.itswinter.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Utility {
+    public static String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public static String getDateTimeFromToday(int day) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd", Locale.getDefault());
+        Date date = new Date();
+        // LocalDateTime.from(date.toInstant()).plusDays(day);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, day);
+        date = c.getTime();
+        return dateFormat.format(date);
+    }
+}
