1970cf2771d84d36c0463738d2d938679ec5e78b
zackeryBrennan <65740435+zackeryBrennan@users.noreply.github.com>
2020-11-13 22:20:04 -0500

    Started getting api

--- app/src/main/java/mobilesdkdemo/rbbn/itswinter/event/EventHomeActivity.java
+++ app/src/main/java/mobilesdkdemo/rbbn/itswinter/event/EventHomeActivity.java
@@ -4,8 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import mobilesdkdemo.rbbn.itswinter.R;
 
@@ -18,6 +28,11 @@ public class EventHomeActivity extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         actionBar.setTitle("Event Schedule");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button searchButton = findViewById(R.id.e_searchButton);
+        searchButton.setOnClickListener(x->{
+           search();
+        });
     }
 
     @Override
@@ -30,5 +45,45 @@ public class EventHomeActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void search(){
+        EditText editText = findViewById(R.id.e_searchBar);
+        String searchTerm = editText.getText().toString();
+
+        EventQuery query = new EventQuery();
+        String apiKey = "KiOshiJsVO1WxmGWXYxpwy4Yxd7Cu6r1";
+        query.execute("https://app.ticketmaster.com/discovery/v2/events.json?apikey="+apiKey+"&city="+searchTerm+"&radius=100");
+
+    }
+
+    private class EventQuery extends AsyncTask<String,Integer,String> {
+
+        protected String doInBackground(String...args) {
+            try{
+                URL url = new URL(args[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream response = urlConnection.getInputStream();
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput(response,"UTF-8");
+                String parameter = null;
+                int eventType = xpp.getEventType();
+                while(eventType != XmlPullParser.END_DOCUMENT){
+
+                }
+            }catch(Exception e){}
+
+            return "Done";
+        }
+
+        protected void onProgressUpdate(Integer... value) {
+
+        }
+
+        protected void onPostExecute(String s){
+
+        }
     }
 }
