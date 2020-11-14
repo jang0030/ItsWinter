754f4542ed786031d497980242a538be6bfe1e20
Kiwoong Andrew Kim <kw2446@gmail.com>
2020-11-14 12:07:28 -0500

    update utility

--- app/src/main/java/mobilesdkdemo/rbbn/itswinter/utility/Utility.java
+++ app/src/main/java/mobilesdkdemo/rbbn/itswinter/utility/Utility.java
@@ -1,12 +1,24 @@
 package mobilesdkdemo.rbbn.itswinter.utility;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class Utility {
-    public static String getDateTime() {
+    public static String getCurrentDateTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat(
                 "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
         Date date = new Date();
@@ -24,4 +36,58 @@ public class Utility {
         date = c.getTime();
         return dateFormat.format(date);
     }
+
+    public static String getCurrentMonth(){
+        return  getCurrentDateTime().substring(0,7);
+    }
+
+    public static void preventTwoClick(final View view){
+        view.setEnabled(false);
+        view.postDelayed(new Runnable() {
+            public void run() {
+                view.setEnabled(true);
+            }
+        }, 1000);
+    }
+
+    public static Intent getWebViewIntent(String url) {
+        return new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    }
+
+    public static void showSnackbar(View v,String str){
+        Snackbar.make(v, str, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    public static void ShowAlertDialog(Activity from, DialogInterface.OnClickListener clickListener, DialogInterface.OnClickListener cancelListener){
+
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                from);
+
+        // set dialog message
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("OK", clickListener);
+        alertDialogBuilder.setNegativeButton("Cancel", cancelListener);
+        alertDialogBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+
+                });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
 }
